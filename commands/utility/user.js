1ea0d44d7de7a1445374dbeaffe244cc7b2026c7
raph <ghostlyraph@proton.me>
2024-02-28 09:25:05 +0100

    general update

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,10 +1,12 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('user')
-		.setDescription('simple user information command'),
-	async execute(interaction) {
-		await interaction.reply(`user - ${interaction.user.username}, joined - ${interaction.member.joinedAt}.`);
-	},
+  data: new SlashCommandBuilder()
+    .setName("user")
+    .setDescription("simple user information command"),
+  async execute(interaction) {
+    await interaction.reply(
+      `user - ${interaction.user.username}, joined - ${interaction.member.joinedAt}.`
+    );
+  },
 };
