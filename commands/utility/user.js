48d433bc401e6958e2fc7d44f979ac2b93225827
raph <ghostlyraph@proton.me>
2024-02-28 08:04:57 +0100

    index updated deploy-command added for easy command deployment user and ping commands added for testing purposes

---
+++ commands/utility/user.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('user')
+		.setDescription('simple user information command'),
+	async execute(interaction) {
+		await interaction.reply(`user - ${interaction.user.username}, joined - ${interaction.member.joinedAt}.`);
+	},
+};
